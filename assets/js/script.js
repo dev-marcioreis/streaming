9872e28d868c224d82a51159ff5a39bd8576c94e
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-16 09:55:08 -0300

    Fix: GSAP for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
-gsap.from('.home__content', {opacity: 0, duration: 1, delay: 1, x: 30})
-gsap.from('.home__image', {opacity: 0, duration: 1, delay: 1, x: -30})
+gsap.from('.home__content', {opacity: 0, duration: 1, delay: 1, x: 10})
+gsap.from('.home__image', {opacity: 0, duration: 1, delay: 1, x: -10})
 
-gsap.from('.info__text', {opacity: 0, duration: 1, delay: 2, y: 30})
-gsap.from('.info__content', {opacity: 0, duration: 1, delay: 2, y: -60})
-
-gsap.from('.cta__left', {opacity: 0, duration: 1, delay: 3, x: 30})
-gsap.from('.cta__right', {opacity: 0, duration: 1, delay: 3, x: -30})
-gsap.from('.cta__info', {opacity: 0, duration: 1, delay: 4, y: 30})
-
-gsap.from('.p__text', {opacity: 0, duration: 1, delay: 4.5, y: -30})
-
-gsap.from('.free__text', {opacity: 0, duration: 1, delay: 5, x: 30})
-gsap.from('.free__image', {opacity: 0, duration: 1, delay: 5, x: -30})
 
